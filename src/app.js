3587f622ba0284cf3ddb2c5031cf95d80ce84ec3
misraag <jaiofficial50@gmail.com>
2026-01-29 23:43:57 +0530

    removing extra code

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
-import { renderSongs, renderPlaylists, renderView } from "./ui.js";
+import { renderPlaylists, renderView } from "./ui.js";
 import { initEvents, initPlaylistEvents } from "./events.js";
 import { loadState, setView } from "./state.js";
 
 loadState();
 renderPlaylists();
-// renderSongs()
 setView("HOME");
 renderView();
 initEvents();
--- src/dom.js
+++ src/dom.js
@@ -1,57 +1,59 @@
-export const songGrid = document.getElementById("songGrid");
-export const audioPlayer = document.getElementById("audioPlayer");
-export const playToggleButton = document.querySelector(".controlPlayButton");
-export const nextButton = document.getElementById("next");
-export const prevButton = document.getElementById("previous");
-export const songCard = document.querySelector(".song-card");
+// NAVBAR ELEMENTS----------------------------
+export const searchBar = document.querySelector(".searchBar");
+export const logoSection = document.querySelector(".logoSection");
 
+// MIDSECTION ELEMENTS-------------LEFT-----------
 export const homeLibrary = document.getElementById("homeLibrary");
 export const exploreBtn = document.getElementById("explore");
 export const libraryBtn = document.getElementById("library");
 export const createPlaylistBtn = document.getElementById("createPlaylist");
 export const dynamicPlaylist = document.getElementById("dynamicPlaylist");
 
-export const searchBar = document.querySelector(".searchBar");
-export const logoSection = document.querySelector(".logoSection");
 
-// Footer info
+// MIDSECTION ELEMENTS-------------RIGHT-----------
+export const backBtn = document.querySelector("#backBtn");
+export const songGrid = document.getElementById("songGrid");
+export const songCard = document.querySelector(".song-card");
+
+
+
+// FOOTER ELEMENTS------------------LEFT------------
+export const audioPlayer = document.getElementById("audioPlayer");
+export const playToggleButton = document.querySelector(".controlPlayButton");
+export const nextButton = document.getElementById("next");
+export const prevButton = document.getElementById("previous");
+
+//------------------------- FOOTER ELEMENTS------------------RIGHT-----------
 export const footerSongTitle = document.querySelector(".songTitle");
 export const footerSongDescription = document.querySelector(".songDescription");
 export const footerSongImage = document.querySelector(".songImage img");
 
-// Volume Section
-export const volumeSlider = document.getElementById("myVolumeBar");
-export const repeatButton = document.querySelector(".fa-repeat");
-export const volumeIcon = document.getElementById("volumeIcon");
+        // Volume Section
+        export const volumeSlider = document.getElementById("myVolumeBar");
+        export const repeatButton = document.querySelector(".fa-repeat");
+        export const volumeIcon = document.getElementById("volumeIcon");
 
-// TIMER SECTION
-export const timeDisplay = document.querySelector(".songTimer");
-export const progressBar = document.querySelector("#progressBar");
-export const seekBar = document.querySelector("#seekBar");
-
-
-// MODAL SECTION FOR CREATING NEW PLAYLIST
-export const playlistModal = document.getElementById("playlistModal");
-export const playlistInput = document.getElementById("playlistInput");
-export const savePlaylistBtn = document.getElementById("savePlaylist");
-export const cancelPlaylistBtn = document.getElementById("cancelPlaylist");
+        // TIMER SECTION
+        export const timeDisplay = document.querySelector(".songTimer");
+        export const progressBar = document.querySelector("#progressBar");
+        export const seekBar = document.querySelector("#seekBar");
 
 
-//MODAL FOR ADDING SONGS TO PLAYLIST
-export const addingSongsModal = document.querySelector("#addingSongsModal");
-export const modalList = document.querySelector("#modalList");
-export const modalCreate = document.querySelector("#modalCreate");
-export const footerMenu = document.querySelector("#footerMenu");
-export const cancelAddingSongs = document.getElementById("cancelAddingSongs");
+        // MODAL SECTION FOR CREATING NEW PLAYLIST
+        export const playlistModal = document.getElementById("playlistModal");
+        export const playlistInput = document.getElementById("playlistInput");
+        export const savePlaylistBtn = document.getElementById("savePlaylist");
+        export const cancelPlaylistBtn = document.getElementById("cancelPlaylist");
 
 
-//PLAYING SONGS FROM CARDS
-// export const playCardSong = document.querySelector(".song-card");
+        //MODAL FOR ADDING SONGS TO PLAYLIST
+        export const addingSongsModal = document.querySelector("#addingSongsModal");
+        export const modalList = document.querySelector("#modalList");
+        export const modalCreate = document.querySelector("#modalCreate");
+        export const footerMenu = document.querySelector("#footerMenu");
+        export const cancelAddingSongs = document.getElementById("cancelAddingSongs");
 
-//Search functionality
-// export const searchBar = document.querySelector(".searchBar");
 
-export const backBtn = document.querySelector("#backBtn");
 
 
 
